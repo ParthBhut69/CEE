4789a4cfa5ebcf76f490c73abd34f17ae3561802
ParthBhut69 <parthbhut69@gmail.com>
2024-11-04 21:39:51 +0530

    Update index.tsx

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -87,7 +87,7 @@ const Hero = () => {
         <div className="flex flex-col-reverse lg:flex-row lg:items-center lg:gap-8 xl:gap-32.5">
           <div className="md:w-1/2 flex flex-col items-center text-center">
             <h1 className="mb-5 text-3xl font-bold text-black dark:text-white xl:text-hero">
-              Empowering Trading{" "}
+              Empowering Trading Journey with AI by{" "}
               <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark">
                 Coin Earn Exchange
               </span>
@@ -114,7 +114,7 @@ const Hero = () => {
               </form>
             </div>
           </div>
-
+          <br></br><br></br>
           <div className="animate_right hidden md:w-1/2 lg:block">
             <motion.div
               className="relative aspect-[600/650]"
